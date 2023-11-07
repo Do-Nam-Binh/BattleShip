9697c7bf747a87d6429524b9db8ffdb6f940c1d0
Do Nam Binh <131113311+Do-Nam-Binh@users.noreply.github.com>
2023-11-07 15:20:56 +0700

    Visualized board on html

--- factories/gameBoardDOM.js
+++ factories/gameBoardDOM.js
@@ -2,18 +2,19 @@ function createCell(index, hasShip = null) {
   const cell = document.createElement("div");
   cell.classList.add(`cell`);
   cell.classList.add(`${index}`);
-  if (hasShip) {
+  if (hasShip == true) {
     cell.classList.add("hasShip");
   }
+  cell.textContent = `${index}`;
   return cell;
 }
 
-function createBoard(boardObj) {
+function createBoard(boardArr) {
   const board = document.createElement("div");
   board.classList.add("board");
   for (let i = 0; i < 100; i++) {
-    if (boardObj[i].hasShip == true) {
-      board.appendChild(createCell(i), true);
+    if (boardArr[i].hasShip) {
+      board.appendChild(createCell(i, true));
     } else {
       board.appendChild(createCell(i));
     }
