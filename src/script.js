4c21fc961b8c7caede7f814e94dd118510e8ac3a
Do Nam Binh <131113311+Do-Nam-Binh@users.noreply.github.com>
2023-11-10 10:16:51 +0700

    Test function on html

--- src/script.js
+++ src/script.js
@@ -1,9 +1,19 @@
-const createBoard = require("../factories/gameBoardDOM");
+const {
+  createBoard,
+  updateBoard,
+  placeShipDOM,
+} = require("../factories/gameBoardDOM");
 const gameSetup = require("../factories/gameSetup");
+const ship = require("../factories/ship.js");
 
 const playerBoardDom = document.querySelector(".player");
 const computerBoardDom = document.querySelector(".computer");
 const game = gameSetup();
 
-playerBoardDom.appendChild(createBoard(game.playerBoard()));
-computerBoardDom.appendChild(createBoard(game.computerBoard()));
+playerBoardDom.appendChild(createBoard(game.playerBoard(), true));
+computerBoardDom.appendChild(createBoard(game.computerBoard(), false));
+updateBoard("playerCell", game.playerBoard());
+updateBoard("computerCell", game.computerBoard());
+
+const shipTest = ship("test", 3);
+placeShipDOM(shipTest, 72, "y", game.playerBoardObj(), "playerCell");
