674a1679be5144ed3fa3871fab9d4c865bcb1f19
Do Nam Binh <131113311+Do-Nam-Binh@users.noreply.github.com>
2023-11-11 10:50:56 +0700

    Add elements to add ship

--- src/script.js
+++ src/script.js
@@ -15,5 +15,29 @@ computerBoardDom.appendChild(createBoard(game.computerBoard(), false));
 updateBoard("playerCell", game.playerBoard());
 updateBoard("computerCell", game.computerBoard());
 
-const shipTest = ship("test", 3);
-placeShipDOM(shipTest, 72, "y", game.playerBoardObj(), "playerCell");
+const shipInput = document.querySelector(".shipInput");
+const locationInput = document.querySelector(".locationInput");
+const axisBtn = document.querySelector(".axis");
+const placeShipBtn = document.querySelector(".placeShip");
+const form = document.querySelector("#placeShipForm");
+
+axisBtn.addEventListener("click", () => {
+  if (axisBtn.textContent == "X") {
+    axisBtn.textContent = "Y";
+  } else {
+    axisBtn.textContent = "X";
+  }
+});
+
+placeShipBtn.addEventListener("click", () => {
+  const shipHolder = ship("", shipInput.value);
+  placeShipDOM(
+    shipHolder,
+    Number.parseInt(locationInput.value),
+    axisBtn.textContent.toLowerCase(),
+    game.playerBoardObj(),
+    "playerCell"
+  );
+});
+// const shipTest = ship("test", 3);
+// placeShipDOM(shipTest, 72, "y", game.playerBoardObj(), "playerCell");
